26a161c5a319d1bb0f2f9e339fcdb768694177da
IamFive <iampurse@vip.qq.com>
2018-10-31 17:14:58 +0800

    Add BMC global constants

---
+++ common/Constants.ps1
@@ -0,0 +1,17 @@
+$global:BMC = @{
+
+  'V52V3Mapping' = @{
+    'HardDiskDrive'='HDD';
+    'DVDROMDrive'='Cd';
+    'PXE'='Pxe';
+    'Others'='Others';
+  };
+
+  'V32V5Mapping' = @{
+    'HDD'='HardDiskDrive';
+    'Cd'='DVDROMDrive';
+    'Pxe'='PXE';
+    'Others'='Others';
+  }
+
+}
