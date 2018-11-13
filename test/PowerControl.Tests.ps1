dbc46c55c1049c8f5ffe1b01ff229038932549c1
IamFive <iampurse@vip.qq.com>
2018-11-13 11:49:19 +0800

    optimize OS to Server

--- test/PowerControl.Tests.ps1
+++ test/PowerControl.Tests.ps1
@@ -4,7 +4,7 @@ Describe "PowerControl" {
   It "set os" {
     try {
       $session = Connect-iBMC -Address 112.93.129.9 -Username chajian -Password "chajian12#$" -TrustCert
-      Set-iBMCSystemPower $session On
+      Set-iBMCServerPower $session On
     }
     finally {
       Disconnect-iBMC $session
