ccbf9ae62df219bfd1e59184dd20cf6e0fd3a040
IamFive <iampurse@vip.qq.com>
2018-12-01 21:41:07 +0800

    optimize asset tag unittests

---
+++ test/invoke-pester.ps1
@@ -0,0 +1,8 @@
+Invoke-Pester -Script "$PSScriptRoot\Connection.Tests.ps1"
+Invoke-Pester -Script "$PSScriptRoot\User.Tests.ps1"
+Invoke-Pester -Script "$PSScriptRoot\Deploy.Tests.ps1"
+Invoke-Pester -Script "$PSScriptRoot\Service.Tests.ps1"
+Invoke-Pester -Script "$PSScriptRoot\SNMP.Tests.ps1"
+Invoke-Pester -Script "$PSScriptRoot\SMTP.Tests.ps1"
+Invoke-Pester -Script "$PSScriptRoot\Syslog.Tests.ps1"
+Invoke-Pester -Script "$PSScriptRoot\AssetTag.Tests.ps1"
