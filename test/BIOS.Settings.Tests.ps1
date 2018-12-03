c66027f3b6b06ad1833c7c5e2019262de7ab3015
IamFive <iampurse@vip.qq.com>
2018-12-03 10:53:27 +0800

    optimize bios settings unittests

--- test/BIOS.Settings.Tests.ps1
+++ test/BIOS.Settings.Tests.ps1
@@ -4,18 +4,30 @@ Describe "BIOS settings features" {
   It "Export&Import From NFS" {
     try {
       $session = Connect-iBMC -Address 112.93.129.9,112.93.129.96 -Username chajian -Password "chajian12#$" -TrustCert
-      Export-iBMCBIOSSetting $session 'nfs://115.159.160.190/data/nfs/9.xml'
-      Import-iBMCBIOSSetting $session 'nfs://115.159.160.190/data/nfs/9.xml'
+      Export-iBMCBIOSSetting $session 'nfs://115.159.160.190/data/nfs/9.xml','nfs://115.159.160.190/data/nfs/96.xml'
+      Import-iBMCBIOSSetting $session 'nfs://115.159.160.190/data/nfs/9.xml','nfs://115.159.160.190/data/nfs/96.xml'
     }
     finally {
       Disconnect-iBMC $session
     }
   }
 
+  It "Export&Import From BMC TEMP" {
+    try {
+      $session = Connect-iBMC -Address 112.93.129.9,112.93.129.96 -Username chajian -Password "chajian12#$" -TrustCert
+      Export-iBMCBIOSSetting $session '/tmp/bios.xml'
+      Import-iBMCBIOSSetting $session '/tmp/bios.xml'
+    }
+    finally {
+      Disconnect-iBMC $session
+    }
+  }
+
+
   It "Import from local file" {
     try {
       $session = Connect-iBMC -Address 112.93.129.9,112.93.129.96 -Username chajian -Password "chajian12#$" -TrustCert
-      Import-iBMCBIOSSetting $session 'C:\Users\Woo\Desktop\config2.xml'
+      Import-iBMCBIOSSetting $session 'C:\Users\Woo\Desktop\9.xml'
     }
     finally {
       Disconnect-iBMC $session
